0fc7ac58bd3821b37f8bd250e18d2533e5ee589e
VusisibiyaDev <68845231+vusidev@users.noreply.github.com>
2021-06-30 19:17:56 +0200

    Add files via upload

---
+++ sticker.js
@@ -0,0 +1,75 @@
+
+
+//create h1
+const h1 = document.createElement("h1");
+h1.setAttribute('id',"h1")
+h1.innerHTML = "7 Days Stickers";
+document.querySelector("body").appendChild(h1);
+
+
+//create h1
+const p = document.createElement("p");
+p.setAttribute('id',"p");
+p.innerHTML = "Write in your on style";
+document.querySelector("body").appendChild(p);
+
+
+
+
+//create input
+const mon = document.createElement("input");
+mon.setAttribute('type',"text");
+mon.setAttribute('placeholder',"#Monday");
+mon.setAttribute('id',"mon");
+document.querySelector("body").appendChild(mon);
+
+
+//create input
+const tue = document.createElement("input");
+tue.setAttribute('type',"text");
+tue.setAttribute('placeholder',"#Tuesday");
+tue.setAttribute('id',"tue");
+document.querySelector("body").appendChild(tue);
+
+
+//create input
+const wen = document.createElement("input");
+wen.setAttribute('type',"text");
+wen.setAttribute('placeholder',"#Wendsday");
+wen.setAttribute('id',"wen");
+document.querySelector("body").appendChild(wen);
+
+
+//create input
+const thu = document.createElement("input");
+thu.setAttribute('type',"text");
+thu.setAttribute('placeholder',"#Thursday");
+thu.setAttribute('id',"thur");
+document.querySelector("body").appendChild(thu);
+
+
+
+//create input
+const fri = document.createElement("input");
+fri.setAttribute('type',"text");
+fri.setAttribute('placeholder',"#Friday");
+fri.setAttribute('id',"fri");
+document.querySelector("body").appendChild(fri);
+
+
+
+//create input
+const sat = document.createElement("input");
+sat.setAttribute('type',"text");
+sat.setAttribute('placeholder',"#Saturday");
+sat.setAttribute('id',"sat");
+document.querySelector("body").appendChild(sat);
+
+
+
+//create input
+const sun = document.createElement("input");
+sun.setAttribute('type',"text");
+sun.setAttribute('placeholder',"#Sunday");
+sun.setAttribute('id',"sun");
+document.querySelector("body").appendChild(sun);
